5ddb53681c7224a841512350cdc1c48da66d94df
samjwu <sjwu@ualberta.ca>
2023-04-11 20:18:18 -0600

    phoneme encoding

--- main.py
+++ main.py
@@ -23,14 +23,21 @@ symbols = "_-!'(),.:;? abcdefghijklmnopqrstuvwxyz"
 symbol_table = {s: i for i, s in enumerate(symbols)}
 symbols = set(symbols)
 
-sample_text = "Hello world! Text to speech!"
+sample_text = "TextToSpeech example: Hello World"
 print(f"Original Text: {sample_text}")
 print(f"Encoded Text: {text_to_sequence(sample_text, symbol_table)}\n")
 
-# Pretrained model: https://pytorch.org/audio/main/pipelines.html#id60
-processor = torchaudio.pipelines.TACOTRON2_WAVERNN_CHAR_LJSPEECH.get_text_processor()
-processed, lengths = processor(sample_text)
+# Pretrained models: https://pytorch.org/audio/main/pipelines.html#id60
 
-print(f"Processed Text: {processed}")
-tokens = [processor.tokens[i] for i in processed[0, : lengths[0]]]
-print(f"Tokens: {tokens}")
+# character encoding
+processor_wavernn_char = torchaudio.pipelines.TACOTRON2_WAVERNN_CHAR_LJSPEECH.get_text_processor()
+processed_wavernn_char, lengths = processor_wavernn_char(sample_text)
+print(f"Character Encoding: {processed_wavernn_char}")
+tokens = [processor_wavernn_char.tokens[i] for i in processed_wavernn_char[0, : lengths[0]]]
+print(f"Tokens: {tokens}\n")
+
+# phoneme encoding
+processor_wavernn_phon = torchaudio.pipelines.TACOTRON2_WAVERNN_PHONE_LJSPEECH.get_text_processor()
+with torch.inference_mode():
+    processed_wavernn_phon, _ = processor_wavernn_phon(sample_text)
+print(f"Phoneme Encoding: {processed_wavernn_phon}")
